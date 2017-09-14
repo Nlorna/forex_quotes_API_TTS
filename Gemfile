5e1c29a92ddd2cda7f005f5c0f7926cab0b8ef58
Lorna Nyakundi <owner@Lornas-MacBook-Air.local>
2017-09-14 09:17:16 -0700

    updating db

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 
-
+gem 'figaro'
 gem 'bootstrap-sass'
 gem 'jquery-rails'
 gem 'httparty'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
     @response = HTTParty.get('http://api.fixer.io/latest?base=USD')
-    businessinsider_response = HTTParty.get("https://newsapi.org/v1/articles?source=business-insider&sortBy=top&apiKey=553f9d4cc4c44a6bba00774325c1a216")
-
+    businessinsider_response = HTTParty.get("https://newsapi.org/v1/articles?source=business-insider&sortBy=top&apiKey=#{ENV['NEWS_API']}")
+NEWS_API
     @articles = businessinsider_response['articles']
   end
 end
