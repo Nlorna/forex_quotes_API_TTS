822ae09597398c1423f4bfc960423ff09291dba2
Lorna Nyakundi <owner@Lornas-MacBook-Air.local>
2017-09-14 09:35:10 -0700

    updatingtoSQL

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @response = HTTParty.get('http://api.fixer.io/latest?base=USD')
     businessinsider_response = HTTParty.get("https://newsapi.org/v1/articles?source=business-insider&sortBy=top&apiKey=#{ENV['NEWS_API']}")
-NEWS_API
+
     @articles = businessinsider_response['articles']
   end
 end
